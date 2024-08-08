6dde1c21ce4e985c8d72ab9d5deef19ce7173195
Matenza <164885821+Matenza@users.noreply.github.com>
2024-08-08 15:00:54 +0000

    Update gpt.py

--- gpt.py
+++ gpt.py
@@ -1,6 +1,8 @@
 import nltk
 #nltk.download('all')
 nltk.download('punkt')
+nltk.download('wordnet')
+
 #nltk.download('averaged_perceptron_tagger')
 from nltk.tokenize import word_tokenize, sent_tokenize
 from nltk.corpus import stopwords
